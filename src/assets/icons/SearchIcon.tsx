a88ada9bb9b55c282ab5119ea4187e2d0bfbe41b
Jude Martin <jude.martin@mic-software.net>
2023-03-13 21:24:28 -0700

    Added AppBar mui

---
+++ src/assets/icons/SearchIcon.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function SearchIcon() {
+    return (
+        <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M9.16797 16.666C13.3101 16.666 16.668 13.3082 16.668 9.16601C16.668 5.02388 13.3101 1.66602 9.16797 1.66602C5.02583 1.66602 1.66797 5.02388 1.66797 9.16601C1.66797 13.3082 5.02583 16.666 9.16797 16.666Z"
+                stroke="#808080"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M15.775 17.2413C16.2167 18.5747 17.225 18.708 18 17.5413C18.7083 16.4747 18.2417 15.5997 16.9583 15.5997C16.0084 15.5913 15.475 16.333 15.775 17.2413Z"
+                stroke="#808080"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    );
+}
+
+export default SearchIcon;
