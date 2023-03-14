7eb35178f78a0cb0f418fcc339f47e8f5e922b33
Jude Martin <jude.martin@mic-software.net>
2023-03-14 16:53:35 +0000

    Merged PR 1080: App Bar - Using MUI
    * installed mui packages
    * added AppBar for app navigation
    
    ![jobfactor-appbar.png](https://dev.azure.com/mic-software/1d456c82-740b-49b0-a18e-ba1b60074291/_apis/git/repositories/3182693e-02f9-47a7-a26a-ceff4268dca4/pullRequests/1080/attachments/jobfactor-appbar.png)

---
+++ src/assets/icons/ArrowDown.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function ArrowDown() {
+    return (
+        <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M14.9336 6.81641H9.74195H5.06695C4.26695 6.81641 3.86695 7.78307 4.43361 8.34974L8.75028 12.6664C9.44195 13.3581 10.5669 13.3581 11.2586 12.6664L12.9003 11.0247L15.5753 8.34974C16.1336 7.78307 15.7336 6.81641 14.9336 6.81641Z"
+                fill="#735722"
+            />
+        </svg>
+    );
+}
+
+export default ArrowDown;
