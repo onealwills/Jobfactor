16b6d9f857606f84148cdb2036b13bfbd058722a
Michael Ajanaku <michael@mic-software.net>
2023-04-13 17:31:01 +0500

    added connection submenu

--- src/components/Navigation/SideNav.tsx
+++ src/components/Navigation/SideNav.tsx
@@ -11,6 +11,10 @@ import SupportIcon from '../../assets/icons/SupportIcon';
 import LogoutIcon from '../../assets/icons/LogoutIcon';
 import SideNavItem from './SideNavItem';
 import { styled } from '@mui/material/styles';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import ArrowLeftRoundedIcon from '@mui/icons-material/ArrowLeftRounded';
+import { useEffect, useState } from 'react';
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
     open?: boolean;
@@ -84,18 +88,101 @@ const sideNavOptions = [
 ];
 
 function SideNav() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const [hideMenu, setHideMenu] = useState(false);
+
+    useEffect(() => {
+        if (location.pathname === '/connections' || location.pathname === '/pending-connections') {
+            setHideMenu(true);
+        } else {
+            setHideMenu(false);
+        }
+    }, [location.pathname]);
     return (
         <>
             <Box
                 sx={{
                     mt: '12px',
                     backgroundColor: 'white',
-                    overflowY: 'auto', // add scroll on y-axis
+                    overflowY: 'auto', // add scroll on y-axis                    
+                    display: 'flex'
                 }}
             >
-                {sideNavOptions.map((nav, index) => (
-                    <SideNavItem nav={nav} index={index} key={index} />
-                ))}
+                <Box
+                    sx={{
+                        width: hideMenu ? '63px' : '100%',
+                        overflow: hideMenu ? 'hidden' : 'unset',
+                    }}
+                >
+                    {sideNavOptions.map((nav, index) => (
+                        <SideNavItem nav={nav} index={index} key={index} />
+                    ))}
+                </Box>
+                <Box sx={{ background: '#FCFBF8', width: '100%', borderRadius: '4px',display: hideMenu ? 'block' : 'none' }}>
+                    <Button
+                        variant='contained'
+                        sx={{
+                            backgroundColor: 'transparent',
+                            boxShadow: 'none',
+                            color: '#494949',
+                            fontFamily: 'Open Sans',
+                            fontWeight: '700',
+                            fontSize: '14px',
+                            width: '100%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            padding: '12px',
+                            '&:hover': {
+                                backgroundColor: '#FFFAF1',
+                                boxShadow: 'none',
+                            }
+                        }}
+                        onClick={() => setHideMenu(false)}
+                    >
+                        COMMUNITY
+                        <ArrowLeftRoundedIcon sx={{ color: '#808080', fontSize: '20px' }} />
+                    </Button>
+                    <Box
+                        sx={{
+                            p: '10px 16px',
+                            fontFamily: 'Open Sans',
+                            '&:hover, &:focus': {
+                                backgroundColor: location.pathname === '/connections' ? '#05668D' : '#FFFAF1',
+                                cursor: 'pointer',
+                                fontWeight: 700,
+                                color: location.pathname === '/connections' ? '#FFFFFF' : '#05668D',
+                            },
+                            backgroundColor: location.pathname === '/connections' ? '#05668D' : 'transparent',
+                            fontWeight: location.pathname === '/connections' ? '700' : '400',
+                            fontSize: '14px',
+                            color: location.pathname === '/connections' ? '#FFFFFF' : '#808080',
+                        }}
+                        onClick={() => navigate('/connections')}
+                    >
+                        My followers
+                    </Box>
+                    <Box
+                        onClick={() => navigate('/pending-connections')}
+                        sx={{
+                            p: '10px 16px',
+                            fontFamily: 'Open Sans',
+                            fontSize: '14px',
+                            '&:hover, &:focus': {
+                                backgroundColor: location.pathname === '/pending-connections' ? '#05668D' : '#FFFAF1',
+                                cursor: 'pointer',
+                                fontWeight: 700,
+                                color: location.pathname === '/pending-connections' ? '#FFFFFF' : '#05668D',
+                            },
+                            backgroundColor: location.pathname === '/pending-connections' ? '#05668D' : 'transparent',
+                            color: location.pathname === '/pending-connections' ? '#FFFFFF' : '#808080',
+                            fontWeight: location.pathname === '/pending-connections' ? '700' : '400',
+                        }}
+                    >
+                        Pending followers
+                    </Box>
+                </Box>
             </Box>
         </>
     );
--- src/components/Navigation/SideNavItem.tsx
+++ src/components/Navigation/SideNavItem.tsx
@@ -14,6 +14,7 @@ export default function SideNavItem(props: {
 }): JSX.Element {
     const hoverRef = useRef(null);
     const [isSelected, setIsSelected] = useState(false);
+    const [hideMenu, setHideMenu] = useState(false);
     const isHover = useHover(hoverRef);
     const { nav, index } = props;
     const navigate = useNavigate();
@@ -23,9 +24,12 @@ export default function SideNavItem(props: {
     useEffect(() => {
         if (nav.route === location.pathname) {
             setIsSelected(true);
+        } else if (location.pathname === '/pending-connections' && nav.route === '/connections') {
+            setIsSelected(true);
         } else {
             setIsSelected(false);
         }
+
     }, [location.pathname, nav.route]);
 
     const handleClick = () => {
@@ -33,6 +37,11 @@ export default function SideNavItem(props: {
             signOut();
             navigate('/login');
         } else {
+            if (location.pathname === '/connections' || location.pathname === '/pending-connections') {
+                setHideMenu(true);
+            } else {
+                setHideMenu(false);
+            }
             navigate(nav.route);
             setIsSelected(true);
         }
@@ -45,6 +54,7 @@ export default function SideNavItem(props: {
             key={index}
             sx={{
                 overflowY: 'auto', // add scroll on y-axis
+                overflow: hideMenu ? 'hidden' : 'unset',
                 height: 60,
                 display: 'flex',
                 alignItems: 'center',
@@ -66,11 +76,14 @@ export default function SideNavItem(props: {
                 fontWeight: isSelected ? 700 : 'normal',
                 color: isSelected ? '#05668D' : '#808080',
                 textTransform: 'uppercase',
+                '& svg': {
+                    minWidth: '32px'
+                }
             }}
         >
             <Box
                 sx={{
-                    height: 55,
+                    height: 60,                    
                     width: 5,
                     backgroundColor: isSelected ? '#05668D' : 'transparent',
                     opacity: isSelected || isHover ? 1 : 0,
