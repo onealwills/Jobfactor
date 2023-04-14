38f0bbeef4ef8be525432b6c372b077860765983
Michael Ajanaku <michael@mic-software.net>
2023-04-14 15:34:56 +0500

    small-authentication-change

--- src/routing/PrivateRoutes.tsx
+++ src/routing/PrivateRoutes.tsx
@@ -16,7 +16,7 @@ function RequireAuth({ children }: { children: React.ReactNode }) {
     const { isAuthenticated } = useAuth();
     const location = useLocation();
 
-    return !isAuthenticated ? (
+    return isAuthenticated ? (
         <>{children}</>
     ) : (
         <Navigate to="/login" replace state={{ path: location.pathname }} />
