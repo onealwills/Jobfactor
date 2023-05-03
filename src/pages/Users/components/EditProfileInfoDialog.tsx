ef35738eef04229f34c91113b206c2b332ec44de
Ryan Prescott <rppgoat@gmail.com>
2023-05-03 14:21:28 -0400

    use already made component

--- src/pages/Users/components/EditProfileInfoDialog.tsx
+++ src/pages/Users/components/EditProfileInfoDialog.tsx
@@ -1,34 +1,109 @@
-import { Grid } from '@mui/material';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import InputLabel from '@mui/material/InputLabel';
+import InputBase from '@mui/material/InputBase';
+import EmailFormIcon from '../../../assets/icons/EmailFormIcon';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+
 import CustomDialog from './CustomDialog';
 
 import List from '@mui/material/List';
 
 import ProfileInfoListItem from './ProfileInfoListItem';
-import Input from '../../common/Input';
+
 import PhoneIcon from '@mui/icons-material/Phone';
 
-const EditProfileInfoDialog = () => (<CustomDialog open={true} title="Ronald Richard">
-    <List
-      sx={{
-        width: '100%',
-      }}
-      component="nav"
-    >
-      
-      <ProfileInfoListItem title="Your Profile" subtitle="https://Ronaldrichald.info" />
-      <ProfileInfoListItem title="Email" subtitle="Ronaldrichie@outlook.com" />
-    </List>
-    <Grid>
-        <Grid item columns={7}>
+const EditProfileInfoDialog = () => {
 
-        </Grid>
-        <Grid item columns={5}>
-            
-        </Grid>
-        <Input InputProps={{
-          startAdornment: (<PhoneIcon />)
-        }}></Input>
-    </Grid>
-</CustomDialog>)
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid, errors }
+  } = useForm();
+
+  return (
+    <CustomDialog open={true} title="Ronald Richard">
+      <List
+        sx={{
+          width: '100%',
+        }}
+        component="nav"
+      >
+        
+        <ProfileInfoListItem title="Your Profile" subtitle="https://Ronaldrichald.info" />
+        <ProfileInfoListItem title="Email" subtitle="Ronaldrichie@outlook.com" />
+      </List>
+      <Grid>
+          <Grid item columns={7}>
+            <Box sx={{ mt: '32px' }}>
+              <Box sx={{ position: 'relative' }}>
+                  <InputLabel
+                      sx={{
+                          color: '#808080',
+                          fontSize: '14px',
+                          position: 'absolute',
+                          top: '8px',
+                          left: '72px',
+                          zIndex: 1,
+                          fontFamily: 'Open Sans'
+                      }}
+                      htmlFor="email"
+                  >
+                      Email
+                  </InputLabel>
+                  {/* Email Address Input */}
+                  <Controller
+                      name="emailAddress"
+                      control={control}
+                      render={({
+                          field: { onChange, value },
+                          fieldState: { error },
+                          formState
+                      }) => (
+                          <InputBase
+                              required
+                              onChange={onChange}
+                              error={!!errors?.emailAddress}
+                              inputProps={{
+                                  autoComplete: '',
+                                  form: {
+                                      autoComplete: 'off'
+                                  },
+                                  inputMode: 'email'
+                              }}
+                              id="email"
+                              startAdornment={
+                                  <EmailFormIcon />
+                              }
+                              rows={1}
+                              sx={{
+                                  backgroundColor: '#FCFBF8',
+                                  width: '100%',
+                                  height: '70px',
+                                  padding: '0px 16px',
+                                  fontFamily: 'open sans',
+                                  color: '#23282B',
+                                  borderBottom:
+                                      '1px solid #D9D9D9',
+                                  mb: '20px',
+                                  '& 	.MuiInputBase-input': {
+                                      ml: '20px',
+                                      position: 'relative',
+                                      top: '8px'
+                                  }
+                              }}
+                          />
+                      )}
+                  />
+                </Box>
+            </Box>
+          </Grid>
+          <Grid item columns={5}>
+              
+          </Grid>
+
+      </Grid>
+  </CustomDialog>)
+}
 
 export default EditProfileInfoDialog;
