c188ede4ae8562c954d957d2a8084f3a8dcdefce
Ryan Prescott <rppgoat@gmail.com>
2023-05-15 12:12:39 -0400

    Remove unnecessary imports

--- src/pages/Users/UserQualifications/UserQualificationsPage.tsx
+++ src/pages/Users/UserQualifications/UserQualificationsPage.tsx
@@ -1,63 +1,6 @@
-import CircleIcon from '@mui/icons-material/Circle';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-
-import {
-    Box,
-    Typography,
-    IconButton,
-    Grid,
-    Container,
-    Divider,
-    Button,
-    List,
-    ListItem
-} from '@mui/material';
-
-import cover from '../../assets/images/cover.jpg';
-import profile from '../../assets/images/profile-sq.png';
-import cambridge from '../../assets/images/cambridge.jpg';
-import nasa from '../../assets/images/nasa.png';
-import huawei from '../../assets/images/huawei.png';
-import northface from '../../assets/images/northface.png';
-import InfoChip from '../components/InfoChip';
-import Section from '../components/Section';
-import SectionHeading from '../components/SectionHeading';
-import Image from '../../../components/Image';
-import MedalIcon from '../../../assets/icons/MedalIcon';
-import EducationSummary from '../components/EducationSummary';
-import WorkSummary from '../components/WorkSummary';
-import AnalyticsProfile from '../../../assets/icons/AnalyticsProfile';
-import AnalyticsSearch from '../../../assets/icons/AnalyticsSearch';
-import AnalyticsGraph from '../../../assets/icons/AnalyticsGraph';
-import { Ranking } from '../types/Ranking';
-import EditContactInfoDialog from '../components/Modals/EditContactInfoDialog';
-import ScoreCard from '../../../components/ScoreCard.tsx';
-import { useState } from 'react';
-import EditProfileAboutMeDialog from '../components/Modals/EditProfileAboutMeDialog';
-import EditIcon from '../../../assets/icons/EditIcon';
-import EditProfileInfoDialog from '../components/Modals/EditProfileInfoDialog';
+import Container from '@mui/material/Container';
 
 function UserQualificationsPage() {
-    const [openContactInfoModal, setOpenContactInfoModal] = useState(false);
-    const [openAboutMeModal, setOpenAboutMeModal] = useState(false);
-    const [openProfileInfoModal, setOpenProfileInfoModal] = useState(false);
-
-    const handleOnEditAboutMe = () => {
-        setOpenAboutMeModal(true);
-    };
-
-    const handleOnEditContactInfo = () => {
-        setOpenContactInfoModal(true);
-    };
-
-    const handleOnEditProfileInfo = () => {
-        setOpenProfileInfoModal(true);
-    };
-
     return (
         <Container
             style={{
@@ -67,7 +10,7 @@ function UserQualificationsPage() {
                 maxWidth: '100%'
             }}
         >
-
+            
         </Container>
     );
 }
